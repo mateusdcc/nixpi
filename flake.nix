870b6b58c62f42283ede31c2cddfbe909a5817ef
Mateus DCC <mateusdc@proton.me>
2026-08-15 11:12:35 -0300

    feat(flake): wire flake outputs, packages, checks, and formatting

---
+++ flake.nix
@@ -0,0 +1,144 @@
+{
+  description = "Nix-native declarative configuration framework for the Pi coding agent";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+      lib = import ./lib {
+        lib = nixpkgs.lib;
+      };
+    in
+    {
+      inherit lib;
+
+      piModules = {
+        default = import ./modules;
+        core = import ./modules/core/package.nix;
+        profiles = {
+          minimal = import ./modules/profiles/minimal.nix;
+          research = import ./modules/profiles/research.nix;
+        };
+        extensions = {
+          echo = import ./modules/extensions/echo.nix;
+          ripgrep-search = import ./modules/extensions/ripgrep-search.nix;
+          plan-mode = import ./modules/extensions/plan-mode.nix;
+        };
+      };
+
+      homeManagerModules = {
+        default = import ./integrations/home-manager.nix;
+        pi = import ./integrations/home-manager.nix;
+      };
+
+      templates = {
+        standalone = {
+          path = ./templates/standalone;
+          description = "Standalone configured Pi environment";
+        };
+        home-manager = {
+          path = ./templates/home-manager;
+          description = "Home Manager configuration for Pi";
+        };
+        devshell = {
+          path = ./templates/devshell;
+          description = "Project-specific development shell with Pi";
+        };
+      };
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nixpiLib = import ./lib {
+            lib = pkgs.lib;
+            inherit pkgs;
+          };
+          mkExt = nixpiLib.mkPiExtension;
+        in
+        {
+          default = nixpiLib.makePi {
+            inherit pkgs;
+            modules = [
+              {
+                programs.pi = {
+                  enable = true;
+                  settings = {
+                    theme = "dark";
+                  };
+                  extensions = {
+                    echo.enable = true;
+                    ripgrep-search.enable = true;
+                  };
+                };
+              }
+            ];
+          };
+
+          pi-unwrapped = pkgs.pi-coding-agent;
+          echo = pkgs.callPackage ./packages/extensions/echo { mkPiExtension = mkExt; };
+          ripgrep-search = pkgs.callPackage ./packages/extensions/ripgrep-search {
+            mkPiExtension = mkExt;
+          };
+          plan-mode = pkgs.callPackage ./packages/extensions/plan-mode { mkPiExtension = mkExt; };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          configuredPi = self.packages.${system}.default;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              configuredPi
+              pkgs.nixfmt
+              pkgs.git
+              pkgs.ripgrep
+            ];
+          };
+        }
+      );
+
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nixpiLib = import ./lib {
+            lib = pkgs.lib;
+            inherit pkgs;
+          };
+        in
+        {
+          eval-tests = pkgs.callPackage ./tests/eval/eval-test.nix {
+            inherit nixpiLib;
+          };
+          invalid-option-tests = pkgs.callPackage ./tests/eval/invalid-option-test.nix {
+            inherit nixpiLib;
+          };
+          build-tests = pkgs.callPackage ./tests/build/build-test.nix {
+            inherit nixpiLib;
+          };
+          hm-tests = pkgs.callPackage ./tests/integration/hm-test.nix { };
+          e2e-tests = pkgs.callPackage ./tests/e2e/e2e-test.nix {
+            inherit nixpiLib;
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+    };
+}
